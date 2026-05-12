dbabb5e552e8c828482616d952c017d5a4ca2a3b
pmilli444 <145785422+pmilli444@users.noreply.github.com>
2026-05-12 21:02:51 +0200

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  var bouton = document.getElementById('retour-haut');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+      bouton.style.display = 'block';
+    } else {
+      bouton.style.display = 'none';
+    }
+  });
+
+});
