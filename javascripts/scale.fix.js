86f08943102da6448916be1cbe827c1a9bd686d2
pmilli444 <145785422+pmilli444@users.noreply.github.com>
2026-05-12 21:58:24 +0200

    Refactor scroll event listener initialization

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  var bouton = document.getElementById('retour-haut');
-
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-      bouton.style.display = 'block';
-    } else {
-      bouton.style.display = 'none';
-    }
-  });
-
+var bouton = document.getElementById('retour-haut');
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 300) {
+    bouton.style.display = 'block';
+  } else {
+    bouton.style.display = 'none';
+  }
 });
